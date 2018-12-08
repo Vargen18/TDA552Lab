efce851220f3e4b94a02a9eb815bcc9d232d9cdb
Vargen18 <widens@net.chalmers.se>
2018-12-08 23:01:59 +0100

    Created Application (main) and moved relevant methods from CarController to it.

---
+++ src/Application.java
@@ -0,0 +1,49 @@
+import com.company.*;
+import controllers.*;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Application {
+
+    // The delay (ms) corresponds to 59 updates a sec (hz)
+    private final int delay = 17;
+    // The timer is started with an listener (see below) that executes the statements
+    // each step between delays.
+    private Timer timer = new Timer(delay, new TimerListener());
+    private static CarController cc;
+
+    public static void main(String[] args) {
+        // Instance of this class
+        Application application = new Application();
+        //Creating a CarView
+        //CarView view = new CarView("Carsim 1,0", cc);
+        //Creating a CarController
+        cc = new CarController();
+
+        //cc.addVehicle(new Volvo240(0, 0, CardinalDirections.cardinalDirection.NORTH));
+        //cc.addVehicle(new Saab95(100, 0, CardinalDirections.cardinalDirection.NORTH));
+        //cc.addVehicle(new Scania(200, 0, cardinalDirection.NORTH));
+
+        cc.cars.add(new Volvo240(0, 0, CardinalDirections.cardinalDirection.NORTH));
+        cc.cars.add(new Saab95(100, 0, CardinalDirections.cardinalDirection.NORTH));
+        cc.cars.add(new Scania(200, 0, CardinalDirections.cardinalDirection.NORTH.NORTH));
+
+        // Start the timer
+        application.timer.start();
+    }
+
+    /* Each step the TimerListener moves all the cars in the list and tells the
+     * view to update its images. Change this method to your needs.
+     * */
+    private class TimerListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            moveAllCars();
+        }
+    }
+
+    private void moveAllCars(){
+        cc.moveAllCars();
+    }
+}
--- src/controllers/CarController.java
+++ src/controllers/CarController.java
@@ -14,46 +14,14 @@ import java.util.ArrayList;
  */
 
 public class CarController implements CardinalDirections {
-    // member fields:
-
-    // The delay (ms) corresponds to 20 updates a sec (hz)
-    private final int delay = 17;
-    // The timer is started with an listener (see below) that executes the statements
-    // each step between delays.
-    private Timer timer = new Timer(delay, new TimerListener());
 
     // The frame that represents this instance View of the MVC pattern
-    CarView frame;
+    CarView frame = new CarView("CarSim 1.0", this);
     // A list of cars, modify if needed
-    ArrayList<Car> cars = new ArrayList<>();
+    public ArrayList<Car> cars = new ArrayList<>();
 
-    //methods:
 
-    public static void main(String[] args) {
-        // Instance of this class
-        CarController cc = new CarController();
-
-        cc.cars.add(new Volvo240(0, 0, cardinalDirection.NORTH));
-        cc.cars.add(new Saab95(100, 0, cardinalDirection.NORTH));
-        //cc.cars.add(new Scania(200, 0, cardinalDirection.NORTH));
-
-        // Start a new view and send a reference of self
-        cc.frame = new CarView("CarSim 1.0", cc);
-
-        // Start the timer
-        cc.timer.start();
-    }
-
-    /* Each step the TimerListener moves all the cars in the list and tells the
-     * view to update its images. Change this method to your needs.
-     * */
-    private class TimerListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            moveAllCars();
-        }
-    }
-
-    private void moveAllCars(){
+    public void moveAllCars(){
         int i = 0;
         for (Car car : cars) {
             moveAndDrawCar(car, i);
