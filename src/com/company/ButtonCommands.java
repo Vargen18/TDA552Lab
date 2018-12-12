6d8413307bd53fe4b78c87e64b70a3f8bd95c433
Vargen18 <45463154+vargen18@users.noreply.github.com>
2018-12-12 14:18:52 +0100

    Made the ButtonCommands interface that holds different commands that the viewer should have. Controller also recognizes them.

---
+++ src/com/company/ButtonCommands.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public interface ButtonCommands {
+
+    String gas = "Gas Pressed";
+    String brake = "Brake pressed";
+    String turboOn = "Turbo on pressed";
+    String turboOff = "Turbo off pressed";
+    String lowerRamp = "Lower ramp pressed";
+    String raiseRamp = "Raise ramp pressed";
+    String startAll = "Start all pressed";
+    String stopAll = "Stop all pressed";
+
+}
